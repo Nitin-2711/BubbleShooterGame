1820690ef443e8ee75c5d9862c9d5405580d4fc7
Nitin Kumar <104916021+Nitin-2711@users.noreply.github.com>
2026-01-14 11:21:48 +0530

    game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,14 +3,23 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    int x = 100;
+    int y = 500;
+
     public GamePanel() {
         setBackground(Color.BLACK);
+
+        Timer timer = new Timer(16, e -> {
+            y -= 2;        // bubble upar ja rahi hai
+            repaint();    // screen refresh
+        });
+        timer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillOval(100, 100, 40, 40); // sample bubble
+        g.fillOval(x, y, 40, 40);
     }
 }
