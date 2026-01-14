4e4af42c4ce7945fabe084c392e69dd128afb508
Nitin Kumar <104916021+Nitin-2711@users.noreply.github.com>
2026-01-14 11:10:41 +0530

    add

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -1,2 +1,8 @@
 public class CollisionManager {
+    public static boolean isColliding(Bubble b1, Bubble b2) {
+        int dx = b1.x - b2.x;
+        int dy = b1.y - b2.y;
+        int distance = (int) Math.sqrt(dx*dx + dy*dy);
+        return distance < b1.radius * 2;
+    }
 }
